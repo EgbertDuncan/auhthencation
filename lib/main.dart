bc595c2b6e3fe8d6f08833568caa9ad80e140150
EgbertDuncan <18520494@gm.uit.edu.vn>
2024-03-04 11:23:56 +0700

    #Update: Fix some firebase error

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application/wrapper.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
